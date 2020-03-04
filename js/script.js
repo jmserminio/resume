83ce189adf9d6eb5d91e9215e60fadc431d48493
jmserminio <60949496+jmserminio@users.noreply.github.com>
2020-03-04 15:51:16 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,17 +7,19 @@ $(function() {
 			right: -50
 		});
 
+		$('.wrap_inner, footer').addClass('content_blur');
+
 		$('nav').animate({
 			right: 0
 		});
-		
-		$('.wrap_inner, footer').addClass('content_blur');
 		
 		$('body').css('overflow', 'hidden');
 
 	});
 	
 	$('a[href^="#"], .wrap_inner').click(function(e) {
+
+		$('.wrap_inner, footer').removeClass('content_blur');
 		
 		$boxWidth = $('nav').width() + 50;
 
@@ -29,14 +31,12 @@ $(function() {
 			right: 0
 		});
 
+		$('body').css('overflow', 'auto');
+
 		$('html, body').animate({
 			scrollTop: $($.attr(this, 'href')).offset().top
 		}, 800);
 		e.preventDefault();
-
-		$('.wrap_inner, footer').removeClass('content_blur');
-	
-		$('body').css('overflow', 'auto');
 
 	});
 
